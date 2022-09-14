45d939ebeba33b23154c706c21c4507a9499bbea
kevin.alvarado@ucrso.info <103754829+KevinJSS@users.noreply.github.com>
2022-09-14 17:00:35 -0600

    Funcionalidad de vista del acta

--- app/javascript/controllers/nested_form_controller.js
+++ app/javascript/controllers/nested_form_controller.js
@@ -3,14 +3,14 @@ import {
 } from "@hotwired/stimulus"
 
 export default class extends Controller {
-    static targets = ["template", "add_item"];
+    static targets = ["template", "add_item", "divider"];
 
     add_association(event) {
         event.preventDefault();
         var content = '<div class="mt-3">';
         content += this.templateTarget.innerHTML.replace(/TEMPLATE_RECORD/g, Math.floor(Math.random() * 20));
         content += '</div>';
-        this.add_itemTarget.insertAdjacentHTML('afterend', content);
+        this.dividerTarget.insertAdjacentHTML('afterend', content);
     }
 
     remove_association(event) {
